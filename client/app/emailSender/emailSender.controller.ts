007921177dbc0b5ff1608f9c432b42a6f6f96557
liliya0artyuh <liliya0artyukh@gmail.com>
2016-03-05 11:57:47 -0500

    chnaged get method to post for email

--- client/app/emailSender/emailSender.controller.ts
+++ client/app/emailSender/emailSender.controller.ts
@@ -7,13 +7,24 @@
 
         constructor($http, $scope, socket, $rootScope) {
             this.$http = $http;
-
+  //check if $rootScope.userIDglobal is defined else define it
+ if(! $rootScope.userIDglobal ){
+  $rootScope.userIDglobal ;
+    }
             this.to;
             this.subject;
             this.emailBody;
             this.result;
             this.message;
-
+/get calendar id from user ----------------------------
+  paramSerializer: '$httpParamSerializerJQLike';
+if (!this.userIDtemp){
+  console.log("do nothing" );
+  window.location = window.location + "/" +  $rootScope.userIDglobal;
+}else{
+      $rootScope.userIDglobal  = this.userIDtemp ;
+      console.log("$rootScope.userIDglobal 555 = " + $rootScope.userIDglobal);
+}
            
             //get calendar id from user ----------------------------
             paramSerializer: '$httpParamSerializerJQLike';
@@ -31,7 +42,7 @@
         	console.log("-------- this.to -----------" + this.to);
         	console.log("-------- this.emailBody -----------" + this.emailBody);
         	console.log("-------- this.subject -----------" + this.subject);
-            this.$http.get('/api/emails', {to: this.to, emailBody: this.emailBody, subject: this.subject}).then(response => {
+            this.$http.post('/api/emails', {to: this.to, emailBody: this.emailBody, subject: this.subject,  paramSerializer: '$httpParamSerializerJQLike'}).then(response => {
                 this.result = response.data;
                         	console.log("-------- request was sent and succeeded-----------");
                 if(this.result == "sent") {
