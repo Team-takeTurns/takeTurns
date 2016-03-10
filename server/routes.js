47921cf93b9145ff871cd2f4e90ffe84572d5397
Francid <splinzy@yahoo.com>
2016-03-10 02:27:14 -0500

    Updated Add Event

--- server/routes.js
+++ server/routes.js
@@ -7,34 +7,38 @@
 import errors from './components/errors';
 import path from 'path';
 
-export default function(app) {
-  // Insert routes below
-  app.use('/api/emails', require('./api/email'));
-  app.use('/api/calendars', require('./api/calendar'));
-  app.use('/api/users', require('./api/user'));
-  // All undefined asset or api routes should return a 404
-  app.route('/:url(api|auth|components|app|bower_components|assets)/*')
-   .get(errors[404]);
+export default function (app) {
+    // Insert routes below
+    app.use('/api/emails', require('./api/email'));
+    app.use('/api/calendars', require('./api/calendar'));
+    app.use('/api/users', require('./api/user'));
+    // All undefined asset or api routes should return a 404
+    app.route('/:url(api|auth|components|app|bower_components|assets)/*')
+        .get(errors[404]);
 
-  // All other routes should redirect to the index.html
-  app.route('/')
-    .get((req, res) => {
-      res.sendFile(path.resolve(app.get('appPath') + '/index.html'));
-    });
-     app.route('/calendar/:id')
-    .get((req, res) => {
-      res.sendFile(path.resolve(app.get('appPath') + '/calendar.html'));
-    });
-     app.route('/admin/:id')
-    .get((req, res) => {
-      res.sendFile(path.resolve(app.get('appPath') + '/calAdmin.html'));
-    });
-         app.route('/week')
-    .get((req, res) => {
-      res.sendFile(path.resolve(app.get('appPath') + '/calendar.html'));
-    });
-             app.route('/event')
-    .get((req, res) => {
-      res.sendFile(path.resolve(app.get('appPath') + '/calendar.html'));
-    });
+    // All other routes should redirect to the index.html
+    app.route('/')
+        .get((req, res) => {
+            res.sendFile(path.resolve(app.get('appPath') + '/index.html'));
+        });
+    app.route('/calendar/:id')
+        .get((req, res) => {
+            res.sendFile(path.resolve(app.get('appPath') + '/calendar.html'));
+        });
+    app.route('/admin/:id')
+        .get((req, res) => {
+            res.sendFile(path.resolve(app.get('appPath') + '/calAdmin.html'));
+        });
+    app.route('/week')
+        .get((req, res) => {
+            res.sendFile(path.resolve(app.get('appPath') + '/calendar.html'));
+        });
+    app.route('/event')
+        .get((req, res) => {
+            res.sendFile(path.resolve(app.get('appPath') + '/calendar.html'));
+        });
+    app.route('/emailSender')
+        .get((req, res) => {
+            res.sendFile(path.resolve(app.get('appPath') + '/calendar.html'));
+        });
 }
